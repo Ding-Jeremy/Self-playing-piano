480ddfbbfc6e3da5c29dbda2cb3545bce5afc676
Ding-Jeremy <145600655+Ding-Jeremy@users.noreply.github.com>
2026-01-22 16:43:38 +0100

    Test PWM, une led

--- Coils_driver/src/main.cpp
+++ Coils_driver/src/main.cpp
@@ -2,28 +2,57 @@
 * name:     main.cpp
   author:   Ding Jérémy
 */
-#include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Arduino.h>
+#include <Adafruit_PWMServoDriver.h>
+
+// Defines
+#define D_OE_PIN 4
+#define D_SERIAL_BAUD 9600
+// Two boards with different I2C addresses
+Adafruit_PWMServoDriver pwm1 = Adafruit_PWMServoDriver(0x40);
+// Adafruit_PWMServoDriver pwm2 = Adafruit_PWMServoDriver(0x41);
+
+void i2c_scan();
 
 void setup()
 {
-  Serial.begin(9600);
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-  Serial.println("Hell world");
+  Serial.begin(D_SERIAL_BAUD);
+  pwm1.begin();
+  // pwm2.begin();
+  pwm1.setPWMFreq(60); // Typical servo refresh rate
+  pwm1.setPWM(15, 1000, 4095);
+  pwm1.setPWM(1, 1000, 4095);
+  pwm1.setPWM(2, 1000, 4095);
+
+  // pwm2.setPWMFreq(60);
+  i2c_scan();
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  Serial.println("Hello world");
   delay(1000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y)
+/*
+ * Scans for available addresses on the bus.
+ * pass a 1D array of size 127.
+ * 1 = Present
+ * 0 = absent
+ */
+void i2c_scan()
 {
-  return x + y;
+  uint8_t index, error;
+  for (index = 1; index < 128; index++)
+  {
+    Wire.beginTransmission(index);
+    error = Wire.endTransmission();
+
+    if (error == 0)
+    {
+      Serial.print("I2C device found at 0x");
+      Serial.print(index, HEX);
+    }
+  }
 }
