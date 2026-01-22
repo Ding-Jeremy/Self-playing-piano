1aea5a31dfcd52aae1beda0cab8dedd47977a945
Ding-Jeremy <145600655+Ding-Jeremy@users.noreply.github.com>
2026-01-22 15:39:41 +0100

    update serial baud, communication with arduino

--- Coils_driver/src/main.cpp
+++ Coils_driver/src/main.cpp
@@ -1,18 +1,29 @@
+/*
+* name:     main.cpp
+  author:   Ding Jérémy
+*/
 #include <Arduino.h>
 
 // put function declarations here:
 int myFunction(int, int);
 
-void setup() {
+void setup()
+{
+  Serial.begin(9600);
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+  Serial.println("Hell world");
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+  Serial.println("Hello world");
+  delay(1000);
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
+int myFunction(int x, int y)
+{
   return x + y;
 }
